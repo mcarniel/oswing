faab219e5224d04fbe3a4b0c6c3f855b837e51ed
mcarniel <mcarniel@e1d01cdd-142e-0410-ac95-ccc3d9d4cfaf>
2007-07-11 07:02:56 +0000

    git-svn-id: https://oswing.svn.sourceforge.net/svnroot/oswing/trunk@112 e1d01cdd-142e-0410-ac95-ccc3d9d4cfaf

--- srcdemo/demo3/ClientApplication.java
+++ srcdemo/demo3/ClientApplication.java
@@ -95,7 +95,7 @@ public class ClientApplication {
 
         for(int i=0;i<900;i++) {
           stmt.close();
-          stmt = conn.prepareStatement("insert into DEMO3 values('ABC"+i+"',null,"+12+i+0.333+","+1234+i+0.56+",?,'ABC','Y','Y','A"+i+"')");
+          stmt = conn.prepareStatement("insert into DEMO3 values('ABC"+i+"',null,"+12+i+0.33+","+1234+i+0.560+",?,'ABC','Y','Y','A"+i+"')");
           stmt.setObject(1,new java.sql.Date(System.currentTimeMillis()+86400000*i));
           stmt.execute();
         }
